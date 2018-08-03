0632f8d2b0523336eeec7c3726895c6a101e9b71
Greg Rutherford <g.rutherford10@gmail.com>
2018-08-03 11:14:04 +0100

    can return fighter profile from drop down menu

--- public/app.js
+++ public/app.js
@@ -15,18 +15,42 @@ const requestComplete = function(response){
   if(this.status !== 200) return;
   const json = this.responseText;
   fighters = JSON.parse(json);
-  populateSelect(fighters);
+  const activeFighters = _.filter(fighters, ['fighter_status', 'Active'])
+  const sortedFigthers = _.sortBy(activeFighters, 'last_name')
+  populateSelect(sortedFigthers);
+  getFighter(sortedFigthers);
 }
 
 const populateSelect = function(fighters){
   const select = document.getElementById('fighter-menu');
   fighters.forEach(function(fighter, index){
-    if(fighter.fighter_status === 'Active'){
     let option = document.createElement('option');
     option.innerText = fighter.first_name + " " + fighter.last_name;
     option.value = index;
-    select.appendChild(option);}
+    select.appendChild(option);
   })
+}
+
+const getFighter = function(fighters){
+  const selectedFighter = document.querySelector('select')
+  selectedFighter.addEventListener('change', function() {
+    let fighter = fighters[this.value]
+    fighterProfile(fighter);
+  })
+}
+
+const fighterProfile = function(fighter){
+  const div = document.getElementById('fighter-profile')
+  const fighterName = document.createElement('h2')
+  fighterName.innerText = `${fighter.first_name} ${fighter.last_name}`
+  const weightClass = document.createElement('p')
+  weightClass.innerText = `Weight Class: ${fighter.weight_class}`
+  const profilePicture = document.createElement('img')
+  profilePicture.src = fighter.thumbnail
+  div.appendChild(fighterName)
+  div.appendChild(weightClass)
+  div.appendChild(profilePicture)
+  return div;
 }
 
 window.addEventListener('load', app)
